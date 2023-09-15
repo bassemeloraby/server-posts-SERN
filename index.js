2a8cd123d7115aa2cd9748523aa1f5b7be220105
bassem <basem12683@gmail.com>
2023-09-16 00:59:21 +0300

    connect to db

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 
-const colors = require('colors');
+const colors = require("colors");
 const app = express();
-const port = process.env.PORT || '3001';
+const port = process.env.PORT || "3001";
+
+const db = require("./models");
+
 app.use(express.json());
 
+// db.sequelize.sync().then(() => {
+//   app.listen(port, (err) => {
+//     if (err) throw err;
+//     console.log("Server listening on port".blue, port.yellow);
+//   });
+// });
 
-
-
-  app.listen(port, (err) => {
-    if (err) throw err;
-    console.log('Server listening on port'.blue, port.yellow);
-  });  
+db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+      console.log("Server running on port 3001".blue, port.yellow);
+    });
+  });
